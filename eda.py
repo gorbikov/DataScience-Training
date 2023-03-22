736af8715b5535c9c08f3ef682b5c62107321e2e
Denis Gorbikov <denis@gorbikov.ru>
2023-03-22 16:27:51 +0300

    Добавлены сепараторы.

--- eda.py
+++ eda.py
@@ -2,15 +2,49 @@ from pathlib import Path
 import pandas as pd
 
 
+# Выводит разделитель в виде штриховой линии.
+def separator_show(*args: str, type='small'):
+    match type:
+        case "small":
+            print()
+            for ar in args:
+                print(ar)
+            print("---------------------------------------------------------------------")
+        case "large":
+            print()
+            print()
+            for ar in args:
+                print(ar)
+            print("===================================================================================================")
+
+
 # Выводит инфо, сохраняет голову датафрейма в csv в папку intermediate data.
 def data_inspection(df: pd.DataFrame, current_script_name: str, filename: str):
+    separator_show("Информация по " + filename)
     df.info()
     filepath = Path(str("intermediate data/" + current_script_name + "_" + filename + '.csv'))
-    print(filepath)
     filepath.parent.mkdir(parents=True, exist_ok=True)
     df.head().to_csv(filepath)
 
 
-def duplicate_search(df: pd.DataFrame, name: str):
+# Выводит количество дубликатов в датафрейме.
+def duplicates_search(df: pd.DataFrame, name: str):
+    separator_show("Поиск дубликатов в " + name)
     print("Количество дубликатов в " + name + ":")
     print(df[df.duplicated()].shape[0])
+
+
+# Удаляет дубликаты в датафрейме.
+def duplicates_delete(df: pd.DataFrame, name: str):
+    separator_show("Удаление дубликатов в " + name)
+    print("Размер " + name + " до удаления дубликатов: " + str(df.shape))
+    df = df.drop_duplicates()
+    print("Размер " + name + " после удаления дубликатов: " + str(df.shape))
+    return df
+
+
+# Сохраняет датафрейм в виде csv в папку results.
+def intermediate_data_save(df: pd.DataFrame, current_script_name: str, filename: str):
+    filepath = Path(str("results/" + current_script_name + "_" + filename + '.csv'))
+    filepath.parent.mkdir(parents=True, exist_ok=True)
+    df.to_csv(filepath)
--- exam_module_4 - 1/part 1.py
+++ exam_module_4 - 1/part 1.py
@@ -1,63 +1,65 @@
 # Настраиваем импорты.
-import os.path
 import pathlib
-from pathlib import Path
-import pandas as pd
 from eda import *
 
 # Вводные.
-trainPath = Path('data/exam_module_4/1_variant_dna_sequence_mutation_prediction/input/train.csv')
-testPath = Path('data/exam_module_4/1_variant_dna_sequence_mutation_prediction/input/test.csv')
+trainPath = pathlib.Path('data/exam_module_4/1_variant_dna_sequence_mutation_prediction/input/train.csv')
+testPath = pathlib.Path('data/exam_module_4/1_variant_dna_sequence_mutation_prediction/input/test.csv')
 cvFraction = 0.15
 # Фиксируем рандом.
 randomCeed = 777
 # Получаем имя текущего скрипта для сохранения выводов.
 currentScriptName = pathlib.Path(__file__).name
 
-# 1. Определяем тип задачи.
+separator_show('1. Определяем тип задачи.', type="large")
 print('Task type: logistic regression or SVM')
 
-# 2. Создаём фреймы и выделяем часть датасета на CV.
+separator_show("2. Создаём фреймы и выделяем часть датасета на CV.", type="large")
 # Формируем оригинальные датафреймы
 originalTrainDf = pd.read_csv(trainPath, index_col='ID')
 originalTestDf = pd.read_csv(testPath, index_col='ID')
 
-#Смотрим огригинальные датафреймы.
+separator_show("3. Определить тип переменных в датасете.", type="large")
+# Смотрим огригинальные датафреймы.
 data_inspection(originalTrainDf, currentScriptName, "originalTrainDf")
-data_inspection(originalTestDf, currentScriptName, "originalTestDf")
-duplicate_search(originalTrainDf, 'originalTrainDf')
-duplicate_search(originalTestDf, 'originalTestDf')
+duplicates_search(originalTrainDf, "originalTrainDf")
+duplicates_delete(originalTrainDf, "originalTrainDf")
 
-#
-# # Формируем датафреймы из псевдорандомных выборок.
-# trainDf = originalTrainDf.sample(frac=(1 - cvFraction), random_state=randomCeed).drop('mutation', axis=1)
-# trainDfTarget = originalTrainDf.sample(frac=(1 - cvFraction), random_state=randomCeed)[['mutation']]
-# cvDf = originalTrainDf.drop(trainDf.index).drop('mutation', axis=1)
-# cvDfTarget = originalTrainDf.drop(trainDfTarget.index)[['mutation']]
-# testDf = originalTestDf
-#
-# # 4. Если это необходимо провести препроцессинг данных, нужно ли применять алгоритмы понижения размерности?
-# # Нужно ли убирать аномалии?
-# # Переводим первые 9 столбцов в цифры (по методу one-hot, one-hot столбцы добавляются в конце датафрейма).
-# dummieCounter = 0
-# for col in trainDf.columns:
-#     if trainDf[col].dtypes == object:
-#         dummieCounter += len(trainDf[col].unique())
-#         print('Unique in ' + str(col) + ': ' + str(len(trainDf[col].unique())))
-# print('Dummie columns: ' + str(dummieCounter))
-#
-# trainDf = pd.get_dummies(trainDf)
-# cvDf = pd.get_dummies(cvDf)
-# testDf = pd.get_dummies(testDf)
-#
-#
-# # 5. Провести EDA и вывести какие-то умозаключения и посмотреть на распределения признаков, на корреляции, на выбросы.
-#
-# # 6. Подумать над вариантом модели, для того чтобы решить задачу (либо ансамблем моделей)
-#
-# print('Так как n << m, лучше использовать логистическую регрессию, либо SMV without kernel.')
-#
-# # 7. Подумать нужно ли применять Unsupervised learning подход для решения задачи?
-# # Неоходима ли дополнительная информация?
-#
-# # 8. Обучить модель и вывести валидационный скор по метрике качества.
+data_inspection(originalTestDf, currentScriptName, "originalTestDf")
+duplicates_search(originalTestDf, "originalTestDf")
+duplicates_delete(originalTestDf, "originalTestDf")
+
+# Формируем датафреймы из псевдорандомных выборок.
+trainDf = originalTrainDf.sample(frac=(1 - cvFraction), random_state=randomCeed).drop('mutation', axis=1)
+trainDfTarget = originalTrainDf.sample(frac=(1 - cvFraction), random_state=randomCeed)[['mutation']]
+cvDf = originalTrainDf.drop(trainDf.index).drop('mutation', axis=1)
+cvDfTarget = originalTrainDf.drop(trainDfTarget.index)[['mutation']]
+testDf = originalTestDf
+
+# 4. Если это необходимо провести препроцессинг данных, нужно ли применять алгоритмы понижения размерности?
+# Нужно ли убирать аномалии?
+separator_show("""4. Если это необходимо провести препроцессинг данных, нужно ли применять алгоритмы понижения размерности?
+Нужно ли убирать аномалии?""", type="large")
+
+# Переводим первые 9 столбцов в цифры (по методу one-hot, one-hot столбцы добавляются в конце датафрейма).
+dummieCounter = 0
+for col in trainDf.columns:
+    if trainDf[col].dtypes == object:
+        dummieCounter += len(trainDf[col].unique())
+        print('Unique in ' + str(col) + ': ' + str(len(trainDf[col].unique())))
+print('Dummie columns: ' + str(dummieCounter))
+
+trainDf = pd.get_dummies(trainDf)
+cvDf = pd.get_dummies(cvDf)
+testDf = pd.get_dummies(testDf)
+
+# 5. Провести EDA и вывести какие-то умозаключения и посмотреть на распределения признаков, на корреляции, на выбросы.
+
+# 6. Подумать над вариантом модели, для того чтобы решить задачу (либо ансамблем моделей)
+
+print('Так как n << m, лучше использовать логистическую регрессию, либо SMV without kernel.')
+
+# 7. Подумать нужно ли применять Unsupervised learning подход для решения задачи?
+# Неоходима ли дополнительная информация?
+
+# 8. Обучить модель и вывести валидационный скор по метрике качества.
