195e6ce407fc2655df320ae4d123925dbd0875d9
Denis Gorbikov <denis@gorbikov.ru>
2023-03-28 15:53:44 +0300

    Дописал метод generate_correlation_with_target. Теперь все красиво.

--- eda.py
+++ eda.py
@@ -1,11 +1,16 @@
 from pathlib import Path
 
-import matplotlib.pyplot as plt
+import matplotlib
+import matplotlib.pyplot
+import matplotlib.figure
+import matplotlib.axes
+import matplotlib.patches
+import numpy as np
 import pandas as pd
 
 
 def show_separator(*title_texts: str, size='small'):
-    # Выводит разделитель в виде штриховой линии.
+    """Выводит разделитель в виде штриховой линии."""
     match size:
         case "small":
             print()
@@ -21,7 +26,7 @@ def show_separator(*title_texts: str, size='small'):
 
 
 def save_results(current_script_name: str, df: pd.DataFrame, filename: str):
-    # Сохраняет датафрейм в виде csv в папку intermediate data/results/.
+    """Сохраняет датафрейм в виде csv в папку intermediate data/results/."""
     show_separator("Сохраняем датафрейм " + filename + " в csv")
     filepath = Path(str("intermediate data/results/" + current_script_name + "_" + filename + '.csv'))
     filepath.parent.mkdir(parents=True, exist_ok=True)
@@ -30,7 +35,7 @@ def save_results(current_script_name: str, df: pd.DataFrame, filename: str):
 
 
 def inspect_data(current_script_name: str, df: pd.DataFrame, filename: str):
-    # Выводит инфо, сохраняет голову датафрейма в csv в папку intermediate data/heads/.
+    """Выводит инфо, сохраняет голову датафрейма в csv в папку intermediate data/heads/."""
     show_separator("Информация по " + filename)
     df.info()
     filepath = Path(str("intermediate data/heads/" + current_script_name + "_" + filename + '_head.csv'))
@@ -42,23 +47,23 @@ def inspect_data(current_script_name: str, df: pd.DataFrame, filename: str):
 
 
 def search_duplicates(df: pd.DataFrame, df_name: str):
-    # Выводит количество дубликатов в датафрейме.
+    """Выводит количество дубликатов в датафрейме."""
     show_separator("Поиск дубликатов в " + df_name)
     print("Количество дубликатов в " + df_name + ":")
     print(df[df.duplicated()].shape[0])
 
 
-def delete_duplicates(df: pd.DataFrame, df_name: str):
-    # Удаляет дубликаты в датафрейме.
+def delete_duplicates(df: pd.DataFrame, df_name: str) -> pd.DataFrame:
+    """Удаляет дубликаты в датафрейме."""
     show_separator("Удаление дубликатов в " + df_name)
     print("Размер " + df_name + " до удаления дубликатов: " + str(df.shape))
-    df: pd.DataFrame = df.drop_duplicates()
+    df = df.drop_duplicates()
     print("Размер " + df_name + " после удаления дубликатов: " + str(df.shape))
     return df
 
 
 def show_nans(df: pd.DataFrame, df_name: str):
-    # Выводит количество пустых клеток в датафрейме.
+    """Выводит количество пустых клеток в датафрейме."""
     show_separator("Поиск пустых клеток в " + df_name)
     print("Количество строк с пустыми клетками в " + df_name + ":")
     print(df[df.isnull().any(axis=1)].shape[0])
@@ -67,24 +72,27 @@ def show_nans(df: pd.DataFrame, df_name: str):
 
 
 def count_unique_for_object_type(df: pd.DataFrame, df_name: str):
-    # Выбирает столбцы типа object, выводит количество уникальных записей в каждом таком столбце.
+    """Выбирает столбцы типа object, выводит количество уникальных записей в каждом таком столбце."""
     show_separator("Уникальные значения в столбцах типа object в " + df_name)
     dummy_counter = 0
     for col in df.columns:
         if df[col].dtypes == object:
             dummy_counter += len(df[col].unique())
             print('Unique in ' + str(col) + ': ' + str(len(df[col].unique())))
+
     print('Dummy columns: ' + str(dummy_counter))
 
 
-def show_boxplot(current_script_name: str, df: pd.DataFrame, df_name: str, column_name: str):
-    # Выводит график с выбросами и сохраняет в папку "intermediate data/diagrams/".
+def generate_boxplot(current_script_name: str, df: pd.DataFrame, df_name: str, column_name: str):
+    """Сохраняет график с выбросами в папку "intermediate data/diagrams/."""
     show_separator("Распределение значений для столбца " + column_name + " в датафрейме " + df_name)
     current_column = df[[column_name]]
+    plt = matplotlib.pyplot()
     plt.figure(figsize=(19.2, 10.8))
     plt.boxplot(current_column)
     plt.title("Распределение значений для столбца " + column_name + " в датафрейме " + df_name)
     plt.grid()
+    # TODO добавить подписи данных.
     plt.tight_layout()
     filepath = Path(
         str("intermediate data/diagrams/" + current_script_name + "_" + df_name + "_" + column_name + '_boxplot.png'))
@@ -94,14 +102,16 @@ def show_boxplot(current_script_name: str, df: pd.DataFrame, df_name: str, colum
     plt.close()
 
 
-def show_histogram(current_script_name: str, df: pd.DataFrame, df_name: str, column_name: str):
-    # Выводит гистограмму и сохраняет в папку "intermediate data/diagrams/".
+def generate_histogram(current_script_name: str, df: pd.DataFrame, df_name: str, column_name: str):
+    """Сохраняет гистограмму в папку "intermediate data/diagrams/."""
     show_separator("Распределение значений для столбца " + column_name + " в датафрейме " + df_name)
     current_column = df[[column_name]]
+    plt = matplotlib.pyplot()
     plt.figure(figsize=(19.2, 10.8))
     plt.hist(current_column, bins=100)
     plt.title("Гистограмма для столбца " + column_name + " в датафрейме " + df_name)
     plt.grid()
+    # TODO добавить подписи данных.
     plt.tight_layout()
     filepath = Path(
         str("intermediate data/diagrams/" + current_script_name + "_" + df_name + "_" + column_name + '_histogram.png'))
@@ -111,21 +121,49 @@ def show_histogram(current_script_name: str, df: pd.DataFrame, df_name: str, col
     plt.close()
 
 
-def show_correlation_with_target(current_script_name: str, df: pd.DataFrame, df_name: str, column_for_correlation: str):
-    # Выводит матрицу корреляций для датафрейма + рисует столбчатый график, сохраняет в папку intermediate
-    # data/diagrams/.
+def generate_correlation_with_target(current_script_name: str, df: pd.DataFrame, df_name: str,
+                                     column_for_correlation: str):
+    """Выводит матрицу корреляций для датафрейма + сохраняет столбчатый график в папку intermediate data/diagrams/."""
+
+    # Выводит разделитель с описанием того, что делает функция.
     show_separator("Матрица корреляций для " + df_name + " со столбцом " + column_for_correlation)
+
+    # Считает и выводит матрицу корреляций.
     corr_df = df.corr()[[column_for_correlation]].drop(column_for_correlation, axis=0)
     print(corr_df.sort_values(by=[column_for_correlation], ascending=False))
-    plt.figure(figsize=(19.2, 10.8))
 
-    x_axis = corr_df.index.values
-    y_axis = corr_df[[column_for_correlation]].values.reshape(x_axis.shape[0])
-    plt.barh(x_axis, y_axis)
-    plt.title("Матрица корреляций для " + df_name + " со столбцом " + column_for_correlation)
-    plt.grid()
-    plt.tight_layout()
-    filepath = Path(str("intermediate data/diagrams/" + current_script_name + "_" + df_name + '.png'))
+    # Формирует данные для составления графика.
+    plt_labels = corr_df.index.values
+    plt_values = corr_df[[column_for_correlation]].values.reshape(plt_labels.shape[0])
+
+    # Создаёт фигуру, оси и решетку.
+    fig: matplotlib.figure.Figure = matplotlib.pyplot.figure()
+    ax: matplotlib.axes.Axes = matplotlib.pyplot.axes()
+    fig.set_size_inches(w=19.2, h=10.8)
+    ax.grid()
+
+    # Рисует график.
+    ax.set_title("Матрица корреляций для " + df_name + " со столбцом " + column_for_correlation)
+    y_pos = np.arange(len(plt_labels))
+    ax.barh(y_pos, plt_values)
+    ax.set_yticks(y_pos, plt_labels)
+
+    # Добавляет подписи данных.
+    graph_patches = ax.patches
+    for patch, value in zip(graph_patches, plt_values):
+        patch: matplotlib.patches.Rectangle = patch
+        width = patch.get_width()
+        ax.text(width/2, patch.get_y()+patch.get_height()/2, round(value, 2), verticalalignment='center',
+                bbox={'facecolor': 'grey','edgecolor':'None', 'alpha': 0.5, 'pad': 0.3})
+
+    # Регулирует отступы на графике.
+    matplotlib.pyplot.tight_layout()
+
+    # Сохраняет график в файл.
+    filepath = Path(str("intermediate data/diagrams/" + current_script_name + "_" + df_name + '_correlation.png'))
     filepath.parent.mkdir(parents=True, exist_ok=True)
-    plt.savefig(filepath)
-    plt.close()
+    matplotlib.pyplot.savefig(filepath)
+
+    # Убирает фигуру из памяти и закрывает график.
+    fig.clear()
+    matplotlib.pyplot.close()
--- exam_module_4 - 1/part 1.py
+++ exam_module_4 - 1/part 1.py
@@ -13,71 +13,72 @@ cvFraction = 0.15
 # Фиксируем рандом.
 randomCeed = 777
 
-show_separator('1. Определяем тип задачи.', size="large")
-print('Task type: logistic regression or SVM')
+# show_separator('1. Определяем тип задачи.', size="large")
+# print('Task type: logistic regression or SVM')
 
 show_separator("2. Создаём фреймы и выделяем часть датасета на CV.", size="large")
 # Формируем оригинальные датафреймы
 originalTrainCvDf = pd.read_csv(trainPath, index_col='ID')
 originalTestDf = pd.read_csv(testPath, index_col='ID')
 
-show_separator("3. Определить тип переменных в датасете.", size="large")
+# show_separator("3. Определить тип переменных в датасете.", size="large")
 # Смотрим оригинальные датафреймы.
-inspect_data(currentScriptName, originalTrainCvDf, "originalTrainCvDf")
-inspect_data(currentScriptName, originalTestDf, "originalTestDf")
+# inspect_data(currentScriptName, originalTrainCvDf, "originalTrainCvDf")
+# inspect_data(currentScriptName, originalTestDf, "originalTestDf")
 
 # Ищем и удаляем дубликаты в оригинальных данных.
-search_duplicates(originalTrainCvDf, "originalTrainCvDf")
-search_duplicates(originalTestDf, "originalTestDf")
+# search_duplicates(originalTrainCvDf, "originalTrainCvDf")
+# search_duplicates(originalTestDf, "originalTestDf")
+
 
 # Ищем незаполненные и некорректно заполненные данные.
-show_nans(originalTrainCvDf, "originalTrainCvDf")
-show_nans(originalTestDf, "originalTestDf")
+# show_nans(originalTrainCvDf, "originalTrainCvDf")
+# show_nans(originalTestDf, "originalTestDf")
 
 # Ищем аномалии.
-columns = originalTrainCvDf.iloc[:, 9:19].columns.tolist()
-for column in columns:
-    show_histogram(currentScriptName, originalTrainCvDf, "originalTrainCvDf", column)
+# columns = originalTrainCvDf.iloc[:, 9:19].columns.tolist()
+# for column in columns:
+#     generate_histogram(currentScriptName, originalTrainCvDf, "originalTrainCvDf", column)
+#
+# columns = originalTrainCvDf.iloc[:, 30:31].columns.tolist()
+# for column in columns:
+#     generate_histogram(currentScriptName, originalTrainCvDf, "originalTrainCvDf", column)
+#
+# columns = originalTrainCvDf.iloc[:, 19:30].columns.tolist()
+# for column in columns:
+#     generate_boxplot(currentScriptName, originalTrainCvDf, "originalTrainCvDf", column)
+#     generate_histogram(currentScriptName, originalTrainCvDf, "originalTrainCvDf", column)
 
-columns = originalTrainCvDf.iloc[:, 30:31].columns.tolist()
-for column in columns:
-    show_histogram(currentScriptName, originalTrainCvDf, "originalTrainCvDf", column)
-
-columns = originalTrainCvDf.iloc[:, 19:30].columns.tolist()
-for column in columns:
-    show_boxplot(currentScriptName, originalTrainCvDf, "originalTrainCvDf", column)
-    show_histogram(currentScriptName, originalTrainCvDf, "originalTrainCvDf", column)
-
-show_separator("""4. Если это необходимо провести препроцессинг данных, нужно ли применять алгоритмы понижения
-размерности? Нужно ли убирать аномалии?""", size="large")
+# show_separator("""4. Если это необходимо провести препроцессинг данных, нужно ли применять алгоритмы понижения
+# размерности? Нужно ли убирать аномалии?""", size="large")
 
 # Переводим первые 9 столбцов в цифры (по методу one-hot, one-hot столбцы добавляются в конце датафрейма).
-count_unique_for_object_type(originalTrainCvDf, "originalTrainCVDf")
-count_unique_for_object_type(originalTestDf, "originalTestDf")
+# count_unique_for_object_type(originalTrainCvDf, "originalTrainCVDf")
+# count_unique_for_object_type(originalTestDf, "originalTestDf")
 trainCvDf = pd.get_dummies(originalTrainCvDf)
 testDf = pd.get_dummies(originalTestDf)
 
-show_separator("""5. Провести EDA и вывести какие-то умозаключения и посмотреть на распределения признаков, на
-корреляции, на выбросы.""", size='large')
+# show_separator("""5. Провести EDA и вывести какие-то умозаключения и посмотреть на распределения признаков, на
+# корреляции, на выбросы.""", size='large')
 
-show_correlation_with_target(currentScriptName, trainCvDf, "trainCvDf", "mutation")
+generate_correlation_with_target(currentScriptName, trainCvDf, "trainCvDf", "mutation")
 
 # Формируем датафреймы из псевдослучайных выборок.
-trainDf = trainCvDf.sample(frac=(1 - cvFraction), random_state=randomCeed).drop('mutation', axis=1)
-trainDfTarget = trainCvDf.sample(frac=(1 - cvFraction), random_state=randomCeed)[['mutation']]
-cvDf = trainCvDf.drop(trainDf.index).drop('mutation', axis=1)
-cvDfTarget = trainCvDf.drop(trainDfTarget.index)[['mutation']]
+# trainDf = trainCvDf.sample(frac=(1 - cvFraction), random_state=randomCeed).drop('mutation', axis=1)
+# trainDfTarget = trainCvDf.sample(frac=(1 - cvFraction), random_state=randomCeed)[['mutation']]
+# cvDf = trainCvDf.drop(trainDf.index).drop('mutation', axis=1)
+# cvDfTarget = trainCvDf.drop(trainDfTarget.index)[['mutation']]
 
 # Просмотр данных перед сохранением.
-inspect_data(currentScriptName, trainDf, "trainDf")
-inspect_data(currentScriptName, trainDfTarget, "trainDfTarget")
-inspect_data(currentScriptName, cvDf, "cvDf")
-inspect_data(currentScriptName, cvDfTarget, "cvDfTarget")
-inspect_data(currentScriptName, testDf, "testDf")
+# inspect_data(currentScriptName, trainDf, "trainDf")
+# inspect_data(currentScriptName, trainDfTarget, "trainDfTarget")
+# inspect_data(currentScriptName, cvDf, "cvDf")
+# inspect_data(currentScriptName, cvDfTarget, "cvDfTarget")
+# inspect_data(currentScriptName, testDf, "testDf")
 
 # Сохраняем данные в csv.
-save_results(currentScriptName, trainDf, "trainDf")
-save_results(currentScriptName, trainDfTarget, "trainDfTarget")
-save_results(currentScriptName, cvDf, "cvDf")
-save_results(currentScriptName, cvDfTarget, "cvDfTarget")
-save_results(currentScriptName, testDf, "testDf")
+# save_results(currentScriptName, trainDf, "trainDf")
+# save_results(currentScriptName, trainDfTarget, "trainDfTarget")
+# save_results(currentScriptName, cvDf, "cvDf")
+# save_results(currentScriptName, cvDfTarget, "cvDfTarget")
+# save_results(currentScriptName, testDf, "testDf")
