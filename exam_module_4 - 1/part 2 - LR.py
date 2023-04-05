393962e56ee52db7138b3581cb4c7c3ac75544a8
Denis Gorbikov <denis@gorbikov.ru>
2023-04-05 17:54:19 +0300

    В процессе генерации квадратичных фич.

--- exam_module_4 - 1/part 2 - LR.py
+++ exam_module_4 - 1/part 2 - LR.py
@@ -1,5 +1,6 @@
 # Настраиваем импорты.
 import pathlib
+import pandas as pd
 from os import path
 
 import torch
@@ -31,11 +32,47 @@ train_path = pathlib.Path("intermediate data/results/part 1.py_train_df.csv")
 train_target_path = pathlib.Path("intermediate data/results/part 1.py_train_df_target.csv")
 
 # Создаёт датафреймы.
-cv_df = pd.read_csv(cv_path)
-cv_target_df = pd.read_csv(cv_target_path)
-test_df = pd.read_csv(test_path)
-train_df = pd.read_csv(train_path)
-train_target_df = pd.read_csv(train_target_path)
+cv_df = pd.read_csv(cv_path, index_col='ID')
+cv_target_df = pd.read_csv(cv_target_path, index_col='ID')
+test_df = pd.read_csv(test_path, index_col='ID')
+train_df = pd.read_csv(train_path, index_col='ID')
+train_target_df = pd.read_csv(train_target_path, index_col='ID')
+
+
+
+
+
+
+
+
+
+
+
+# Добавляем новые фичи на основе имеющихся.
+for column1 in train_df.loc[:, "A":"U"].columns:
+    for column2 in train_df.loc[:, "A":"U"].columns:
+        new_column = {column1 + "*" + column2: train_df[column1] * train_df[column2]}
+        new_column = pd.DataFrame(new_column)
+        train_df = pd.concat([train_df, new_column], axis=1)
+        print(column1 + "*" + column2 + " Train DONE!")
+
+        new_column = {column1 + "*" + column2: cv_df[column1] * cv_df[column2]}
+        new_column = pd.DataFrame(new_column)
+        cv_df = pd.concat([cv_df, new_column], axis=1)
+        print(column1 + "*" + column2 + " CV DONE!")
+
+
+train_df.head().to_csv(tmp_folder_path.joinpath("train.csv"))
+cv_df.head().to_csv(tmp_folder_path.joinpath("cv.csv"))
+
+
+
+
+
+
+
+
+
 
 show_separator("6. Подумать над вариантом модели, для того чтобы решить задачу (либо ансамблем моделей)",
                size="large")
@@ -84,8 +121,8 @@ class LogisticRegression(torch.nn.Module):
 lr = LogisticRegression(train_tensor.size()[1])
 
 # Задаёт параметры обучения.
-num_epochs = 10000
-learning_rate = 0.01
+num_epochs = 1000
+learning_rate = 0.001
 # Использует Binary Cross Entropy.
 criterion = torch.nn.BCELoss()
 # Использует ADAM optimizer.
@@ -124,16 +161,27 @@ generate_loss_function_graph(current_script_name, loss_function_values_for_graph
 torch.save(lr.state_dict(), results_folder_path.joinpath(current_script_name + '_model_weights'))
 show_separator("Параметры модели сохранены в папке results.")
 
+# Выводит метрики результата для train.
+show_separator("Текущие метрики для train:")
+with torch.no_grad():
+    target_predicted = lr(train_tensor)
+    target_predicted_class = target_predicted.round()
+    acc = target_predicted_class.eq(train_target_tensor).sum() / float(train_target_tensor.shape[0])
+    print(f'accuracy: {acc.item():.4f}')
+    print(classification_report(train_target_tensor, target_predicted_class))
+    confusion_matrix_train = confusion_matrix(train_target_tensor, target_predicted_class)
+    print(confusion_matrix_train)
+
 # Выводит метрики результата для CV.
 show_separator("Текущие метрики для CV:")
 with torch.no_grad():
     target_predicted = lr(cv_tensor)
-target_predicted_class = target_predicted.round()
-acc = target_predicted_class.eq(cv_target_tensor).sum() / float(cv_target_tensor.shape[0])
-print(f'accuracy: {acc.item():.4f}')
-print(classification_report(cv_target_tensor, target_predicted_class))
-confusion_matrix = confusion_matrix(cv_target_tensor, target_predicted_class)
-print(confusion_matrix)
+    target_predicted_class = target_predicted.round()
+    acc = target_predicted_class.eq(cv_target_tensor).sum() / float(cv_target_tensor.shape[0])
+    print(f'accuracy: {acc.item():.4f}')
+    print(classification_report(cv_target_tensor, target_predicted_class))
+    confusion_matrix_cv = confusion_matrix(cv_target_tensor, target_predicted_class)
+    print(confusion_matrix_cv)
 
 # TODO Добиться точности на CV на уровне 95%+.
 # TODO Посмотреть какие метрики могут позволить выявить проблему.
